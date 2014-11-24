aafab84a5a46c75c905622b71b696798b4c6f717
bytegeist <bytegeist@caladan.localdomain>
2014-11-24 20:55:22 +0100

    beautify code

--- ParticleSystem.py
+++ ParticleSystem.py
@@ -12,6 +12,7 @@ class ParticleSystem:
         self.particles.append(first_particle)
 
     def add_particle(self, particle):
+        """add a particle to the system"""
         self.particles.append(particle)
 
     def calculate_distance(self, particle_nb_1, particle_nb_2):
@@ -22,10 +23,13 @@ class ParticleSystem:
     def calculate_distance_matrix(self):
         """returns the distance between all system of the system as a
         matrix"""
-        # moegliche optimierung: nur die untere haelfte der Matrix berechnen und
-        # nach oben spiegeln
+        # possible optimization: calculate only on half of the matrix and mirror
+        # elements
+        # number of particles
         nb_of_system = len(self.particles)
+        # create empty matrix
         distance_matrix = zeros((nb_of_system, nb_of_system))
+        # calculate elements of matrix
         for x in range(nb_of_system):
             for y in range(nb_of_system):
                 distance_matrix[x, y] = self.calculate_distance(x, y)
@@ -33,11 +37,11 @@ class ParticleSystem:
 
     def calculate_direction_vector(self, particle_nb_1, particle_nb_2):
         """calculate direction vector"""
-        # Richtungsvektor
-        xy = self.particles[particle_nb_2].location - self.particles[particle_nb_1].location
-        # Abstand
+        # direction vector
+        xy = (self.particles[particle_nb_2].location -
+              self.particles[particle_nb_1].location)
+        # distance
         distance_xy = self.calculate_distance(particle_nb_1, particle_nb_2)
-        # Einheitsvektor bestimmen
+        # unit vector
         e_xy = xy / distance_xy
-        #print e_xy
         return e_xy
--- run.py
+++ run.py
@@ -1,39 +1,37 @@
 from ParticleSystem import *
 import matplotlib.pyplot as plt
 
+# create three particles
 m1 = Particle(100, array([0, 0, 0]), array([0, 0, 0]))
 m2 = Particle(10, array([10, 10, 0]), array([1, 1, 0]))
 m3 = Particle(150, array([10, 5, 0]), array([1, 5, 0]))
+m4 = Particle(100, array([0, 5, 0]), array([0, 0, 0]))
 
+# setup particle system
 sys = ParticleSystem(m1)
 sys.add_particle(m2)
 sys.add_particle(m3)
+sys.add_particle(m4)
 
+# print masses of particles
 print sys.particles[0].mass
 print sys.particles[1].mass
 print sys.particles[2].mass
 
-print sys.calculate_distance(0, 1)
+# print distance between m0 and m1
+print sys.calculate_distance(0, 3)
 
-nb_part = len(sys.particles)
-matrix = zeros((nb_part, nb_part))
-print matrix
-
-for x in range(nb_part):
-    for y in range(nb_part):
-        matrix[x, y] = sys.calculate_distance(x, y)
-
-print matrix
-
-print "sosososo..."
-
+# print distance matrix:
 print "distance matrix:"
 print sys.calculate_distance_matrix()
 
 print "direction vector:"
+# print direction vector from m0 to m1:
 print sys.calculate_direction_vector(0, 1)
+# print direction vector from m0 to m2:
 print sys.calculate_direction_vector(0, 2)
-
+# print direction vector from m0 to m3:
+print sys.calculate_direction_vector(0, 3)
 
 
 
